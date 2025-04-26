4c1af3fc653ee78c9145fe024ed2c44eeb47a877
sahil16-12 <sahil16december@gmail.com>
2025-04-26 18:20:18 +0530

    sahil eats khavsa daily

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,9 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // raise to 1 MB if needed
     rollupOptions: {
